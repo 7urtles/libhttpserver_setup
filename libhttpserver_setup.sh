6fe314f54cf71bd659179006104896448e5de4a2
chparmley <63277749+chparmley@users.noreply.github.com>
2023-02-20 19:16:36 -0600

    Create libhttpserver_setup.sh

---
+++ libhttpserver_setup.sh
@@ -0,0 +1,20 @@
+sudo apt-get install -y libmicrohttpd-dev
+sudo apt-get install libtool m4 automake
+sudo apt-get install build-essential manpages-dev
+sudo autoupdate
+
+git clone https://github.com/etr/libhttpserver.git
+cd libhttpserver
+
+sudo autoupdate
+sudo ./bootstrap
+
+mkdir build
+cd build
+
+sudo ../configure
+sudo make
+sudo make install
+
+cd ../examples
+g++ hello_world_1.cpp -o hello -l httpserver
